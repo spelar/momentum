48fc7a2d4bded2613e80d3c85d2449b1251d5528
spelar <spelar7@gmail.com>
2018-04-14 16:12:31 +0900

    sass component, randomImage components 추가

---
+++ src/frontend/components/pages/Intro/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RandomImageContainer from 'containers/RandomImageContainer';
+
+const Intro = () => {
+  return (
+    <div>
+      <RandomImageContainer />
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/frontend/containers/RandomImageContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RandomImageContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        영화이미지
+      </div>
+    )
+  }
+}
+
+export default RandomImageContainer;
