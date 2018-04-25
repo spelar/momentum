5c98a873b7ffbe8c34958eb8b8329e664b18c9a6
spelar <spelar7@gmail.com>
2018-04-25 18:51:58 +0900

    heroku 배포 테스트중

--- src/backend/server.js
+++ src/backend/server.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, '../../build/')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'../../build/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.get('/movies/:query', function (req, res) {
@@ -35,3 +35,5 @@ app.get('/movies/:query', function (req, res) {
 app.listen(port);
 
 console.log(`Momentum app listening on port ${port}`);
+
+module.exports = server;
