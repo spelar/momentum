74d72dc87ebc6d9def29eb8f420b14910d236364
spelar <spelar7@gmail.com>
2018-05-17 22:53:53 +0900

    성능 개선을 위하여 함수형 컴포넌트로 변경

--- frontend/src/components/atoms/RandomImage/RandomImage.js
+++ frontend/src/components/atoms/RandomImage/RandomImage.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './RandomImage.scss';
 
-class RandomImage extends Component {
-  render() {
-    const {randomImage} = this.props;
-    return (
-      <div className={styles.randomImage}>
-        <div className={styles.image}>
-          <img src={randomImage} alt="영화 포스터" />
-        </div>
-        <div className={styles.dim}>
 
-        </div>
-        <div className={styles.titleWrap}>
-          <h1 className={styles.title}>
-            관심있는 영화를 <br />
-            검색해 보세요 !
-          </h1>
-          <Link to="/search" className={styles.start}>시작하기</Link>
-        </div>
+const RandomImage = ({randomImage}) => {
+  return (
+    <div className={styles.randomImage}>
+      <div className={styles.image}>
+        <img src={randomImage} alt="영화 포스터" />
       </div>
-    );
-  }
+      <div className={styles.dim}>
+
+      </div>
+      <div className={styles.titleWrap}>
+        <h1 className={styles.title}>
+          관심있는 영화를 <br />
+          검색해 보세요 !
+        </h1>
+        <Link to="/search" className={styles.start}>시작하기</Link>
+      </div>
+    </div>
+  )
 }
 
 RandomImage.propTypes = {
