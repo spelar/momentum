da3ce14888aa43ef3cf6beb889256abd44f982e2
Sungsu Kim <spelar7@gmail.com>
2020-04-02 23:28:05 +0900

    heroku 배포 수정

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -291,10 +291,10 @@ module.exports = function(webpackEnv) {
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         // Allows for better profiling with ReactDevTools
-        ...(isEnvProductionProfile && {
-          'react-dom$': 'react-dom/profiling',
-          'scheduler/tracing': 'scheduler/tracing-profiling',
-        }),
+        // ...(isEnvProductionProfile && {
+        //   'react-dom$': 'react-dom/profiling',
+        //   'scheduler/tracing': 'scheduler/tracing-profiling',
+        // }),
         ...(modules.webpackAliases || {}),
       },
       plugins: [
