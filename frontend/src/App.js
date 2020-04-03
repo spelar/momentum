8d1913106a2645ad3eeb8368011b0cc2f1f4f01f
Sungsu Kim <spelar7@gmail.com>
2020-04-04 00:12:22 +0900

    code splitting 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import RandomImageContainer from './containers/RandomImageContainer';
-import SearchContainer from './containers/SearchContainer';
-import SearchResultContainer from './containers/SearchResultContainer';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+const RandomImageContainer = lazy(() => import('./containers/RandomImageContainer'));
+const SearchContainer = lazy(() => import('./containers/SearchContainer'));
+const SearchResultContainer = lazy(() => import('./containers/SearchResultContainer'));
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route exact path="/" component={RandomImageContainer} />
-        <Route path="/search" component={SearchContainer} />
-        <Route path="/searchResult" component={SearchResultContainer} />
-      </div>
-    );
-  }
-}
+const App = () => (
+	<Router>
+		<Suspense fallback={''}>
+			<Switch>
+				<Route exact path="/" component={RandomImageContainer} />
+				<Route path="/search" component={SearchContainer} />
+				<Route path="/searchResult" component={SearchResultContainer} />
+			</Switch>
+		</Suspense>
+	</Router>
+);
 
 export default App;
