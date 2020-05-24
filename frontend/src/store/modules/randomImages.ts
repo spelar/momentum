d3288c1d7042ef6a72d83e3a25ce0f3133789db3
Sungsu Kim <spelar7@gmail.com>
2020-05-25 06:10:59 +0900

    randomImage module에 typesafe-actions 라이브러리 적용

--- frontend/src/store/modules/randomImages.ts
+++ frontend/src/store/modules/randomImages.ts
@@ -1,4 +1,4 @@
-import { createReducer } from 'typesafe-actions';
+import { createReducer, createAction, ActionType } from 'typesafe-actions';
 import produce from 'immer'; 
 import poster1 from 'static/images/poster1.jpg';
 import poster2 from 'static/images/poster2.jpg';
@@ -12,25 +12,24 @@ export interface RandomImagesState {
 
 const GET_RANDOM_IMAGE = "randomImages/GET_RANDOM_IMAGE";
 
-export const getRandomImage = createAction(GET_RANDOM_IMAGE);
+export const getRandomImage = createAction(GET_RANDOM_IMAGE)<number>();
 
-const initialState = {
+const initialState: RandomImagesState = {
   randomImage: ''
 };
+
+type RandomImagesAction = ActionType<typeof getRandomImage>;
 
 const randomMovieImages = [poster1, poster2, poster3, poster4, poster5];
 
 export const randomImageLength = randomMovieImages.length;
 
-const randomImages = createReducer(
-	{
-		[GET_RANDOM_IMAGE] : (state, action: { payload: number }) => 
-			produce(state, draft => {
-				const randomImage = randomMovieImages[action.payload];
-				draft.randomImage = randomImage;
-			}),
-	},
-	initialState
-);
+const randomImages = createReducer<RandomImagesState, RandomImagesAction>(initialState, {
+	[GET_RANDOM_IMAGE] : (state, action) => 
+		produce(state, draft => {
+			const randomImage = randomMovieImages[action.payload];
+			draft.randomImage = randomImage;
+		}),
+});
 
 export default randomImages;
