24b97731b1a8bba584e754ef31dc07e6f7b100e4
Sungsu Kim <spelar7@gmail.com>
2020-06-24 23:39:15 +0900

    SearchList 수정

--- frontend/src/components/atoms/SearchList/SearchList.tsx
+++ frontend/src/components/atoms/SearchList/SearchList.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './SearchList.scss';
+import SearchListItem from '../SearchlistItem/SearchListItem';
 
 export interface SearchListProps {
 	search: {
@@ -31,36 +32,6 @@ const SearchList = ({search, searchResult, moreMovieClick}: SearchListProps) => 
 		)
 	};
 
-	const makeSearchList = () => {
-		return searchResult.movieList.map((movie, i) => {
-			const makeActorList = () => {
-				let actor = movie.actor.replace(/\|/gi, ", ").slice(0,-2);
-				return (
-					<span dangerouslySetInnerHTML={{__html: actor}}/>
-				)
-			};
-			return (
-				<li key={'movie' + i} className="movie clearFix">
-					<a target="_blank" href={movie.link} rel="noopener noreferrer">
-						{movie.image !== "" ? <div className="image"><img src={movie.image} alt="영화 포스터" /></div> : <div className="image noimage"><span /></div>}
-						<div className="info">
-							<div className="clearFix">
-								<h2 dangerouslySetInnerHTML={{__html: movie.title}} /><span className="pubDate"> (<span dangerouslySetInnerHTML={{__html: movie.pubDate}} />)</span>
-							</div>
-							<div className="userRating">
-								<i className="momentum-icon momentum-icon-star" />
-								<span className="score" dangerouslySetInnerHTML={{__html: movie.userRating}}/>
-							</div>
-							<div className="actor">
-								{makeActorList()}
-							</div>
-						</div>
-					</a>
-				</li>
-			)
-		});
-	};
-
 	return (
 		<div>
 			{search.isAutoComplete !== true ?
@@ -68,7 +39,7 @@ const SearchList = ({search, searchResult, moreMovieClick}: SearchListProps) => 
 				{searchResult.isLoading ? '' : <div className="title">검색 결과</div>}
 				<ul>
 					<div className={searchResult.isLoading ? "loader" : ''}></div>
-					{searchResult.isLoading ? '' : (searchResult.movieList.length === 0 ? makeNoMovieList() : makeSearchList())}
+					{searchResult.isLoading ? '' : (searchResult.movieList.length === 0 ? makeNoMovieList() : searchResult.movieList.map((movie, i) => (<SearchListItem />)))}
 				</ul>
 				{searchResult.movieList.length > 0 && searchResult.isLastMovie === false ?
 					<div className="moreMovie">
