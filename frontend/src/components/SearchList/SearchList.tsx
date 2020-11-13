dbd08bc39bfcb4f5d705fb18295f02654c599b59
Sungsu Kim <spelar7@gmail.com>
2020-11-13 23:15:33 +0900

    SearchList props name 변경

--- frontend/src/components/SearchList/SearchList.tsx
+++ frontend/src/components/SearchList/SearchList.tsx
@@ -41,7 +41,7 @@ const SearchList = ({search, searchResult, moreItemClick}: SearchListProps) => {
 				{searchResult.isLoading ? '' : <div className="title">검색 결과</div>}
 				<ul>
 					<div className={searchResult.isLoading ? "loader" : ''}></div>
-					{searchResult.isLoading ? '' : (searchResult.searchList.length === 0 ? makeNoMovieList() : searchResult.searchList.map((movie, i) => (<SearchListItem movie={movie} key={i} />)))}
+					{searchResult.isLoading ? '' : (searchResult.searchList.length === 0 ? makeNoMovieList() : searchResult.searchList.map((item, i) => (<SearchListItem item={item} key={i} />)))}
 				</ul>
 				{searchResult.searchList.length > 0 && searchResult.isLastMovie === false ?
 					<div className="moreItem">
--- frontend/src/components/SearchListItem/SearchListItem.tsx
+++ frontend/src/components/SearchListItem/SearchListItem.tsx
@@ -3,27 +3,27 @@ import './SearchListItem.scss';
 import { Item } from '../../store/modules/searchResult';
 
 interface SearchListItemProps {
-	movie: Item;
+	item: Item;
 }
 
-const SearchListItem = ({movie}: SearchListItemProps) => {
+const SearchListItem = ({item}: SearchListItemProps) => {
 	const makeActorList = () => {
-		let actor = movie.actor.replace(/\|/gi, ", ").slice(0,-2);
+		let actor = item.actor.replace(/\|/gi, ", ").slice(0,-2);
 		return (
 			<span dangerouslySetInnerHTML={{__html: actor}}/>
 		)
 	};
 	return (
 		<li className="movie clearFix">
-			<a target="_blank" href={movie.link} rel="noopener noreferrer">
-				{movie.image !== "" ? <div className="image"><img src={movie.image} alt="영화 포스터" /></div> : <div className="image noimage"><span /></div>}
+			<a target="_blank" href={item.link} rel="noopener noreferrer">
+				{item.image !== "" ? <div className="image"><img src={item.image} alt="영화 포스터" /></div> : <div className="image noimage"><span /></div>}
 				<div className="info">
 					<div className="clearFix">
-						<h2 dangerouslySetInnerHTML={{__html: movie.title}} /><span className="pubDate"> (<span dangerouslySetInnerHTML={{__html: movie.pubDate}} />)</span>
+						<h2 dangerouslySetInnerHTML={{__html: item.title}} /><span className="pubDate"> (<span dangerouslySetInnerHTML={{__html: item.pubDate}} />)</span>
 					</div>
 					<div className="userRating">
 						<i className="momentum-icon momentum-icon-star" />
-						<span className="score" dangerouslySetInnerHTML={{__html: movie.userRating}}/>
+						<span className="score" dangerouslySetInnerHTML={{__html: item.userRating}}/>
 					</div>
 					<div className="actor">
 						{makeActorList()}
