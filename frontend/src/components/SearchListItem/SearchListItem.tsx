3bf718f51a081b4d52447aa9d49c7acc8ab74e16
Sungsu Kim <spelar7@gmail.com>
2020-12-06 13:29:31 +0900

    검색타입에 따라 검색결과 정보를 다르게 보여줄 수 있도록 리팩토링

--- frontend/src/components/SearchListItem/SearchListItem.tsx
+++ frontend/src/components/SearchListItem/SearchListItem.tsx
@@ -9,26 +9,48 @@ interface SearchListItemProps {
 }
 
 const SearchListItem = ({item, searchType}: SearchListItemProps) => {
-	const makePersonList = () => {
-		let personName = '';
-		if (searchType === 'movie') {
-			personName = item.actor.replace(/\|/gi, ", ").slice(0,-2);
-			return (
-				<span dangerouslySetInnerHTML={{__html: personName}}/>
-			) 
-		} else if (searchType === 'book') {
-		 personName = item.author.replace(/\|/gi, ", ");
-			return (
-				<span dangerouslySetInnerHTML={{__html: personName}}/>
-			)
-		}
-	};
+	// const makePersonList = () => {
+	// 	let personName = '';
+	// 	if (searchType === 'movie') {
+	// 		personName = item.actor.replace(/\|/gi, ", ").slice(0,-2);
+	// 		return (
+	// 			<span dangerouslySetInnerHTML={{__html: personName}}/>
+	// 		) 
+	// 	} else if (searchType === 'book') {
+	// 	 personName = item.author.replace(/\|/gi, ", ");
+	// 		return (
+	// 			<span dangerouslySetInnerHTML={{__html: personName}}/>
+	// 		)
+	// 	}
+	// };
+
+	const makeMovieItem = () => {
+		return (
+			<div>
+				<div className="clearFix">
+					<h2 dangerouslySetInnerHTML={{__html: item.title}} /><span className="pubDate"> (<span dangerouslySetInnerHTML={{__html: item.pubDate}} />)</span>
+				</div>
+				<div className="userRating">
+					<i className="momentum-icon momentum-icon-star" />
+					<span className="score" dangerouslySetInnerHTML={{__html: item.userRating}}/>
+				</div>
+				<div className="person">
+					<span dangerouslySetInnerHTML={{__html: item.actor.replace(/\|/gi, ", ").slice(0,-2)}}/>
+				</div>
+			</div>
+		);
+	}
+
+	const makeBookItem =() => {
+
+	}
 	return (
 		<li className="movie clearFix">
 			<a target="_blank" href={item.link} rel="noopener noreferrer">
 				{item.image !== "" ? <div className="image"><img src={item.image} alt="영화 포스터" /></div> : <div className="image noimage"><span /></div>}
 				<div className="info">
-					<div className="clearFix">
+					{searchType === 'movie' ? makeMovieItem() : makeBookItem()}
+					{/* <div className="clearFix">
 						<h2 dangerouslySetInnerHTML={{__html: item.title}} /><span className="pubDate"> (<span dangerouslySetInnerHTML={{__html: item.pubDate}} />)</span>
 					</div>
 					<div className="userRating">
@@ -37,7 +59,7 @@ const SearchListItem = ({item, searchType}: SearchListItemProps) => {
 					</div>
 					<div className="person">
 						{makePersonList()}
-					</div>
+					</div> */}
 				</div>
 			</a>
 		</li>
