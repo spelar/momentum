52907593dcf996601c6ef4f277890efaf2901ba5
Sungsu Kim <spelar7@gmail.com>
2020-12-31 00:37:19 +0900

    메세지 수정

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -54,7 +54,7 @@ const Header = (props: HeaderProps) => {
 	const searchBtnClick = useCallback(() => {
     dispatch(emptyList());
     if (search.searchKeyword === '') {
-      alert('영화 제목을 입력해주세요.')
+      alert('검색어를 입력해주세요.')
 			dispatch(setLoadingState(true));
       let searchData = {
 				searchKeyword: search.searchKeyword,
