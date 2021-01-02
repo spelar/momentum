c12e6926b1210d71b9329cfc4e66a3f3d50025ec
Sungsu Kim <spelar7@gmail.com>
2021-01-02 12:24:56 +0900

    SearchContainer 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const RandomImageContainer = lazy(() => import('./containers/RandomImageContainer'));
-const SearchContainer = lazy(() => import('./containers/SearchContainer'));
 const SearchResultContainer = lazy(() => import('./containers/SearchResultContainer'));
 const MovieContainer = lazy(() => import('./containers/MovieContainer'));
 const BookContainer = lazy(() => import('./containers/BookContainer'));
@@ -11,7 +10,6 @@ const App = () => (
 		<Suspense fallback={''}>
 			<Switch>
 				<Route exact path="/" component={RandomImageContainer} />
-				<Route path="/search" component={SearchContainer} />
 				<Route path="/searchResult" component={SearchResultContainer} />
 				<Route path="/movie" component={MovieContainer} />
 				<Route path="/book" component={BookContainer} />
