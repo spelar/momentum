1c726601f7c5c5f97851e559dbf56faeeecdca83
Sungsu Kim <spelar7@gmail.com>
2021-01-03 14:30:20 +0900

    클라리언트에서 서버로 request할때 파라미터가 있을때만 응답하도록 수정

--- server.js
+++ server.js
@@ -11,13 +11,18 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.resolve(__dirname, 'frontend/build')));
 
-app.get('/:searchType?', function (req, res) {
+app.get('/:searchType?', function (req, res, next) {
 	let searchType = req.params.searchType;
   let indexParam = "";
+	var query = req.query.query;
+	if (!query) {
+		next();
+		return;
+	}
   if(req.query.start) {
     indexParam = "&start=" + req.query.start;
   }
-  const api_url = 'https://openapi.naver.com/v1/search/' + searchType + '.json?query=' + encodeURI(req.query.query) + '&display=5' + indexParam; // json 결과
+  const api_url = 'https://openapi.naver.com/v1/search/' + searchType + '.json?query=' + encodeURI(query) + '&display=5' + indexParam; // json 결과
   const options = {
     url: api_url,
     headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
